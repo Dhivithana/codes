9eebc0c5bc27fb87a5f560b027c096dd32d74f65
Dhivithana <30549440+Dhivithana@users.noreply.github.com>
2018-03-19 16:50:09 +0530

    Create sum1.c

---
+++ sum1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int k,sum=0,i;
+	printf("\n Enter the number:");
+	scanf("%d",&k);
+	for(i=0;i<=k;i++)
+	{
+		sum=sum+i;
+			
+	}
+	printf("\n The sum is:%d",sum);
+
+	return 0;
+}
