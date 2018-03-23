637138f76b0cc8aa1e555e9150e0912a8e9c7a67
Dhivithana <30549440+Dhivithana@users.noreply.github.com>
2018-03-23 16:56:55 +0530

    Create join.c

---
+++ join.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int a,b;
+printf("\n Enter a:");
+scanf("%d",&a);
+printf("\n enter b:");
+scanf("%d",&b);
+printf("\n The joint number is: %d%d",a,b);
+return 0;
+}
