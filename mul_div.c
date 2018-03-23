84cb0fdd15bc4ec02a5014b284e8587b787565a2
Dhivithana <30549440+Dhivithana@users.noreply.github.com>
2018-03-23 16:01:16 +0530

    Create mul_div.c

---
+++ mul_div.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+	int a,b,c;
+	printf("\n Enter the first number:");
+	scanf("%d",&a);
+	printf("\n Enter the second number:");
+	scanf("%d",&b);
+	printf("\n Enter the third number:");
+	scanf("%d",&c);
+	printf("\n (A*B)/C is: %d",(a*b)/c);
+	return 0;
+}
